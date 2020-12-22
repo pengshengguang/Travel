22efb207fac0358e2f0412858c39a70ec0e16470
pengshengguang <499964903@qq.com>
2020-12-22 22:52:20 +0800

    finish page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive> <!--作用，当路由的内容加载一次之后，我路由的内容就放在内存当中，下一次再进同一个路由时，不需要重新渲染组件，只需要从内存里拿出来显示就行-->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -26,16 +27,20 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
-        .then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city=' + this.city) // 这里加上this.city的原因是，当你进入定位搜索页，选择城市时，会返回主页，主页要根据你选择的城市来显示主页的内容
+        .then(this.getHomeInfoSucc) //                  又由于app.vue里面有keep-alive的原因，mounted这个钩子无法执行，需要通过activated这个钩子来重新根据城市获取主页数据
     },
     getHomeInfoSucc (res) {
       res = res.data
@@ -49,7 +54,14 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () { // keep-alive 使得这个组件再次显示时能执行activated这个生命周期函数！！！！！
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
